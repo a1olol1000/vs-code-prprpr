185004e1f29c605457f3ac7fe8c94a5f7a32c481
Albin Lindqvist <albin.lindqvist@elev.ga.ntig.se>
2022-08-31 14:22:30 +0200

    initial combat

---
+++ ov_1_lucktext/ov_1_lucktext/Program.cs
@@ -0,0 +1,25 @@
+﻿string name;
+string animal;
+string scared;
+string time;
+string pronoun;
+
+Console.WriteLine("vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine("vilket djur är farligast?");
+animal = Console.ReadLine();
+
+Console.WriteLine("vad är du rädd för?");
+scared = Console.ReadLine();
+
+Console.WriteLine("vad är ditt pronun han/hon Etc...?");
+pronoun = Console.ReadLine();
+
+Console.WriteLine("vad är klockan?");
+time = Console.ReadLine();
+
+Console.WriteLine($"{name} blev dödad av 2 {animal} när {pronoun} sprang från {scared} kl {time}!");
+
+
+Console.ReadLine();
